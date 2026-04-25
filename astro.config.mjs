c002dc2a3c5a130da37daca1ad7b0a1543eb71d5
= <pixie_cemodan@hotmail.com>
2026-04-25 11:50:45 -0700

    DEPLOY to CF ready

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,15 @@ import react from "@astrojs/react";
 import partytown from "@astrojs/partytown";
 import sitemap from "@astrojs/sitemap";
 import markdoc from "@astrojs/markdoc";
+import cloudflare from "@astrojs/cloudflare";
 
 export default defineConfig({
+  output: "server",
+  adapter: cloudflare({
+    imageService: 'cloudflare',
+    runtime: { mode: 'off' },
+  }),
+  site: "https://pav-frontend.pages.dev",
   integrations: [react(), partytown(), sitemap(), markdoc()],
 
   i18n: {
